420a19bfa7bbe69733bd09fdf90526703f87686b
Unicorn <unicorn@Unicorns-iMac.local>
2018-04-02 20:01:34 -1000

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+app.post('/api/order', function (req, res) { //get order
+
+});
+
+app.get('/api/orders', function (req, res) { //get all orders
+
+});
+
+app.get('/api/listings', function (req, res) { //gets all listing
+
+});
+
+app.put('/api/order', function (req, res) { //modify order, for employees only, this changes the order status field
+
+});
+
+app.delete('/api/order', function (req, res) { //mark order as canceled, ( DONT DELETE THINGS FROM DA DATABASE )
+
+});
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
