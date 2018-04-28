c324e744e2b1eedb8aba11e8c55b077b51dcf940
Unicorn <unicorncalcs@gmail.com>
2018-04-28 10:10:30 -1000

    added play store link

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var shell = require('shelljs');
+
+if (!shell.which('git')) {
+	shell.echo('Sorry, this script requires git');
+	shell.exit(1);
+}
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,30 +21,43 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-
+app.post('/gitpull', function (req, res, next) {
+	try {
+		console.log('New commit by', req.body.head_commit.committer.username);
+		shell.exec('npm install');
+		if (shell.exec('git pull origin master').code !== 0) {
+			shell.echo('Error: Git pull failed');
+		} else {
+			shell.echo('Successfully pulled git repo');
+		}
+		res.end('1')
+	} catch (e) {
+		res.end('0')
+	}
+});
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
