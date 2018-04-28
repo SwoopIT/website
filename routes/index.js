e3c5bb4dc7418740a959b191dcc6b2387ea938d4
Unicorn <unicorncalcs@gmail.com>
2018-04-27 20:15:35 -1000

    wew

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'SwoopIt' });
-	res.render('privacypolicy', { title: 'Privacy Policy' });
 });
 
 module.exports = router;
