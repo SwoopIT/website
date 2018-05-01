54e97e4098461dc5f7c2ba6b55a9436074be24e7
Unicorn <unicorncalcs@gmail.com>
2018-05-01 08:11:09 -1000

    aa

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SwoopIt' });
+router.get('/', function (req, res, next) {
+	res.render('index', {title: 'SwoopIt'});
 });
 
+router.get('/admin', function (req, res, next) {
+	res.render('admin', {title: 'Admin Console'});
+});
+router.get('/item', function (req, res, next) {
+	res.render('item', {title: 'New Item'});
+});
+router.get('/blacklist', function (req, res, next) {
+	res.render('blacklist', {title: 'Blacklist User'});
+});
+router.get('/items', function (req, res, next) {
+	res.render('items', {title: 'Remove Items'});
+});
 module.exports = router;
