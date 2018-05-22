417b147c410609709fe4c2afab9c808bce59fe02
Your Name <mdean@hpa.edu>
2018-05-21 17:09:33 -1000

    fixed admin page a bit

--- public/js/index.js
+++ public/js/index.js
@@ -69,7 +69,7 @@ async function drawChart() {
 
 	var options = {
 		title: 'SwoopIt User Emails',
-		chartArea: {width: 300, height: 250}
+		chartArea: {width: 450, height: 350}
 	};
 	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
